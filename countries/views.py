6342bf754dbaaa7c49b638f78b9c973b474e1b63
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2025-07-22 22:39:11 +0200

    Add SQLite3

--- countries/views.py
+++ countries/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from countries_project.utils import process_json_data
 from pathlib import Path
+from countries.models import Country
 from django.core.paginator import Paginator
 from urllib.parse import unquote #  раскодирует все символы, закодированные в URL-формате.
 
@@ -9,52 +10,69 @@ JSON_FILE_PATH = Path() / "country-by-languages.json"
 # Create your views here.
 
 def index_page(request):
-    countries_data = process_json_data(JSON_FILE_PATH)
-    total_countries = len(countries_data)
-    # Собираем все языки в один список
-    all_languages = []
-    for country in countries_data:
-        all_languages.extend(country["languages"])
+    # json
+    # countries_data = process_json_data(JSON_FILE_PATH)
+    # total_countries = len(countries_data)
 
-    # Убираем дубликаты с помощью set, затем сортируем
-    unique_languages = sorted(set(all_languages))
-    total_languages = len(unique_languages)
+    # sqlite
+    countries_data = Country.objects.all()
+    total_countries = countries_data.count()
+
+    # Собираем все языки в один список
+    # all_languages = []
+    # for country in countries_data:
+    #     all_languages.extend(country["languages"])
+    #
+    # # Убираем дубликаты с помощью set, затем сортируем
+    # unique_languages = sorted(set(all_languages))
+    # total_languages = len(unique_languages)
 
     context = {'pagename': 'Country by languages',
                'total_countries': total_countries,
-               'total_languages': total_languages}
+               # 'total_languages': total_languages
+                }
     return render(request, 'pages/index.html', context)
 
 def countries_list_view(request):
-    countries_data = process_json_data(JSON_FILE_PATH)
+    # json
+    # countries_data = process_json_data(JSON_FILE_PATH)
+    # sqlite
+    countries_data = Country.objects.all()
+    # print(f"\n\n\n{countries_data}\n\n\n")
 
     # paginator
     paginator = Paginator(countries_data, 20)  # Показывать по 10 сниппетов на странице
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)  # Получаем объект Page для запрошенной страницы
 
-    context = {'pagename': 'List of countries',
+    context = {'pagename': 'List of Countries',
                'page_obj': page_obj,
-               # 'countries_data': countries_data
                 }
     return render(request,'pages/countries_list.html', context)
 
 def country_detail(request, country_name):
-    countries_data = process_json_data(JSON_FILE_PATH)
+    # json
+    # countries_data = process_json_data(JSON_FILE_PATH)
+    # sqlite
+    countries_data = Country.objects.all()
 
     country_name = unquote(country_name) #  раскодирует все символы, закодированные в URL-формате.
-    country_info = next((c for c in countries_data if c["country"] == country_name), None)
+
+    # json
+    # country_info = next((c for c in countries_data if c["country"] == country_name), None)
+    # sqlite
+    country_info = next((c for c in countries_data if c.name == country_name), None)
     # print(f"\n{country_info}\n")
-    if country_info is None:
-        country_info = {
-            "country": country_name,
-            "languages": [],
-            "error": "Дітько! Страна не найдена"
-        }
+    # if country_info is None:
+    #     country_info = {
+    #         "country": country_name,
+    #         "languages": [],
+    #         "error": "Дітько! Страна не найдена"
+    #     }
 
     context = {
         'pagename': country_name,
-        'languages': country_info["languages"],
+        # 'languages': country_info["languages"],
         'country_info': country_info,
     }
     return render(request, 'pages/country.html', context)
@@ -76,7 +94,7 @@ def languages_list_view(request):
     page_obj = paginator.get_page(page_number)  # Получаем объект Page для запрошенной страницы
     # print(f"\nItems on page {page_number}: {list(page_obj)}\n")
     context = {
-        'pagename': 'Languages',
+        'pagename': 'List of Languages',
         'page_obj': page_obj,
     }
     return render(request, 'pages/languages_list.html', context)
